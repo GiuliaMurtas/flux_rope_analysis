6ad4b5a6f49bcfbb2720511f678c4b0aba4343d0
GiuliaMurtas <51166185+GiuliaMurtas@users.noreply.github.com>
2023-01-09 18:01:57 -0700

    Update ionrec.py

--- ionrec.py
+++ ionrec.py
@@ -12,7 +12,7 @@ ion = []
 rec = []
 time = []
 
-for t in range(0,27):
+for t in range(0,1):
     print(t)
     ds=pipreadmods.pipread(filename,tstep=t,vararrin=['ro_p','ro_n'])
     
@@ -43,3 +43,73 @@ ax.set_xlabel('t')
 plt.legend()
 plt.show()
 plt.savefig("ion_rec.png")
+
+## 2D maps of ionization rate ##
+
+for t in range(27,28):
+    ds=pipreadmods.pipread(filename,tstep=t,vararrin=['ro_p','ro_n'])
+
+    fig,(ax)=plt.subplots(1,1,dpi=300)
+    ax.axis('equal')
+    ax.set(xlim=(-1, 1), ylim=(-1, 1))
+    fig.set_size_inches(9.7,6.0)
+    
+    cp=ax.contourf(ds['xgrid'],ds['ygrid'],ds['ion'][551,:,:],levels=101,cmap='Blues',vmin=0,vmax=1)
+    
+    for item in ([ax.xaxis.label, ax.yaxis.label] +
+                ax.get_xticklabels() + ax.get_yticklabels()):
+        item.set_fontsize(12)
+    ax.title.set_fontsize(16)
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.82, 0.2, 0.02, 0.6])
+    cbar = fig.colorbar(cp, cax=cbar_ax,label='$\Gamma_{ion}$')
+    ax.set_title('t ='+str(t))
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+    if t in range (0,10):
+        savename=("ion_PIP1_00"+str(t)+".jpeg")
+        plt.savefig(savename)
+    if t in range (10,100):
+        savename=("ion_PIP1_0"+str(t)+".jpeg")
+        plt.savefig(savename)
+    if t in range (100,1000):
+        savename=("ion_PIP1_"+str(t)+".jpeg")
+        plt.savefig(savename)
+    plt.close('all')
+
+## 2D maps of recombination rate ##
+
+for t in range(27,28):
+    ds=pipreadmods.pipread(filename,tstep=t,vararrin=['ro_p','ro_n'])
+
+    fig,(ax)=plt.subplots(1,1,dpi=300)
+    ax.axis('equal')
+    ax.set(xlim=(-1, 1), ylim=(-1, 1))
+    fig.set_size_inches(9.7,6.0)
+    
+    cp=ax.contourf(ds['xgrid'],ds['ygrid'],ds['rec'][551,:,:],levels=101,cmap='RdPu',vmin=0,vmax=1)
+    
+    for item in ([ax.xaxis.label, ax.yaxis.label] +
+                ax.get_xticklabels() + ax.get_yticklabels()):
+        item.set_fontsize(12)
+    ax.title.set_fontsize(16)
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.82, 0.2, 0.02, 0.6])
+    cbar = fig.colorbar(cp, cax=cbar_ax,label='$\Gamma_{rec}$')
+    ax.set_title('t ='+str(t))
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+    if t in range (0,10):
+        savename=("rec_PIP1_00"+str(t)+".jpeg")
+        plt.savefig(savename)
+    if t in range (10,100):
+        savename=("rec_PIP1_0"+str(t)+".jpeg")
+        plt.savefig(savename)
+    if t in range (100,1000):
+        savename=("rec_PIP1_"+str(t)+".jpeg")
+        plt.savefig(savename)
+    plt.close('all')
