27cd595596e7bffdea25a50bca72c81513558069
GiuliaMurtas <51166185+GiuliaMurtas@users.noreply.github.com>
2024-03-12 11:07:07 -0600

    Updates on growth_rate_plot.py
    The plot has been included and saved separately

--- growth_rate_plot.py
+++ growth_rate_plot.py
@@ -6,6 +6,27 @@ import matplotlib.pyplot as plt
 import pipreadmods
 import h5py
 
+def growth_rate_plot(filename, show_plot=False):
+
+    filename = "growth_rate_PIP_1.h5"
+    with h5py.File(filename, "r") as f:
+        print("Keys: %s" % f.keys())
+
+        # Get the data
+        time=np.array(f['time'])
+        ke=np.array(f['ke'])
+        data={'time':time,'ke':ke}
+        return(data)
+    
+    rect = [0.16, 0.16, 0.8, 0.8]
+    fig = plt.figure(figsize=[3.5, 2.5])
+    ax = fig.add_axes(rect)
+    plt.plot(time, ke,linestyle='dashed',linewidth=1,color='black')
+    
+    if not show_plot:
+        plt.close()
+
 filename = "growth_rate_PIP_1.h5"
-with h5py.File(filename, "r") as f:
-    print("Keys: %s" % f.keys())
+growth_rate_plot(filename,show_plot=True)
+plt.savefig("growth_rate_PIP_1.jpg")
+plt.close()
